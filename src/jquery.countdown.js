2b447a68fdc794e249b1033fac5b9faf2c208052
Edson Hilios <edson.hilios@telefonicabeta.com>
2013-10-28 14:44:49 -0200

    Trying to not forget anyone

--- src/jquery.countdown.js
+++ src/jquery.countdown.js
@@ -23,7 +23,7 @@
  * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
- // AMD support
+ // AMD support (Thanks to @FagnerMartinsBrack)
 ;(function(factory) {
     if (typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
