f8563ca614465cc8077012c002ca0dd95d8ec92a
Edson Hilios <edson.hilios@gmail.com>
2013-10-31 21:15:18 -0200

    Add the most important test of all that check the calculations math

--- test/countdown_test.js
+++ test/countdown_test.js
@@ -85,6 +85,25 @@ test('allow the callback be setted uppon initialization (legacy style)', functio
     });
 });
 
+module('Plugin math');
+
+asyncTest('time offset calculation', 5, function() {
+    var testDate = new Date().valueOf(); 
+    testDate += 7 * 24 * 60 * 60 * 1000; // 1 week
+    testDate += 2 * 24 * 60 * 60 * 1000; // 2 days
+    testDate += 3 * 60 * 60 * 1000;      // 3 hours
+    testDate += 4 * 60 * 1000;           // 4 minutes
+    testDate += 5 * 1000;                // 5 seconds
+    $dom.countdown(testDate, function(event) {
+        ok(event.offset.weeks   === 1);
+        ok(event.offset.days    === 2);
+        ok(event.offset.hours   === 3);
+        ok(event.offset.minutes === 4);
+        ok(event.offset.seconds === 5);
+        start();
+    });
+});
+
 module('Commands and functionality');
 
 asyncTest('stop/start the countdown', 2, function() {
